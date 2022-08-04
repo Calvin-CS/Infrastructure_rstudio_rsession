ea2e72831028133fae5b75f57a49e19381326c10
Chris Wieringa <cwieri39@calvin.edu>
2022-08-04 09:47:45 -0400

    update Rstudio version

--- inc/install-Rstudio.sh
+++ inc/install-Rstudio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install Rstudio
-export RSW_VERSION=2022.07.0-548.pro5
+export RSW_VERSION=2022.07.1-554.pro3
 export RSW_NAME=rstudio-workbench
 export RSW_DOWNLOAD_URL=https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64
 
@@ -11,4 +11,9 @@ gdebi -n ${RSW_NAME}-${RSW_VERSION_URL}-amd64.deb
 rm ${RSW_NAME}-${RSW_VERSION_URL}-amd64.deb
 apt autoremove -y
 apt clean
-rm -rf /var/lib/rstudio-server/r-versions
+rm -f /var/lib/rstudio-server/r-versions
+
+# drop conf files
+cp -f r-versions /var/lib/rstudio-server/
+cp -f r-versions /etc/rstudio/
+cp -f rsession-profile /etc/rstudio/
