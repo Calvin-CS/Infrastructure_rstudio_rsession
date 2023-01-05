d6d56f6befffa8d4c35fb32de623e690e18f5ded
Chris Wieringa <cwieri39@calvin.edu>
2023-01-05 14:46:24 -0500

    updated debug output

--- inc/cs-populate-users.sh
+++ inc/cs-populate-users.sh
@@ -3,9 +3,11 @@ PASSWD=/opt/passwd/cspasswd
 GROUP=/opt/passwd/csgroup
 
 if test -f "$PASSWD"; then
+	echo "Populating /etc/passwd with CS entries"
 	cat $PASSWD >> /etc/passwd
 fi
 
 if test -f "$GROUP"; then
+	echo "Populating /etc/group with CS entries"
 	cat $GROUP >> /etc/group
 fi
