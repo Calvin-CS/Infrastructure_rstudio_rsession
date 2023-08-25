21457d76cca8cb3e93d99e7d6f4a60d90b2cff9a
root <root@r.cs.calvin.edu>
2023-08-25 18:00:20 +0000

    tune cache script

--- kubernetes/cache-rsession-on-nodes.sh
+++ kubernetes/cache-rsession-on-nodes.sh
@@ -5,6 +5,7 @@
 #    Meant to be run in cron every few hours. Logs to /tmp/cache-rsession-log-$DATETIME.txt
 
 export LOGNAME=/tmp/cache-rsession-log-`date +%Y%m%d%k%M%S`.txt
+export LOCALDIR=`dirname $0`
 
 {
     for NODE in $(/snap/bin/kubectl get nodes | /usr/bin/grep rstudio | /usr/bin/awk '{print $1;}')
@@ -15,7 +16,7 @@ export LOGNAME=/tmp/cache-rsession-log-`date +%Y%m%d%k%M%S`.txt
         
         # use helm to deploy on the specified node
         echo "Installing rsession-daily on node ${NODE}"
-        /snap/bin/helm upgrade --install --create-namespace --atomic --wait --namespace rstudio rsession-${DATETIME} ./rsession --set kubernetes.node=${NODE}
+        /snap/bin/helm upgrade --install --create-namespace --atomic --wait --namespace rstudio rsession-${DATETIME} ${LOCALDIR}/rsession --set kubernetes.node=${NODE}
         /usr/bin/sleep 5
         echo " -- uninstalling now!"
         /snap/bin/helm uninstall rsession-${DATETIME} --namespace rstudio
